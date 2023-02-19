ef2434288b58922f24ead36ad40c825fc3bc8b78
KishanThakrar <kishant2012@gmail.com>
2023-02-19 14:43:37 +0530

    Add Validation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,14 @@
       <b-field label="Value 1">
         <b-input type="number" v-model="value1"></b-input>
       </b-field>
+      <div class="message">{{ validation.firstError("value1") }}</div>
       <b-field label="Value 2">
         <b-input type="number" v-model="value2"></b-input>
       </b-field>
+      <div class="message">{{ validation.firstError("value2") }}</div>
 
       <b-field label="Select Operation">
-        <b-dropdown aria-role="list">
+        <b-dropdown :disabled="buttonDisable" aria-role="list">
           <template #trigger="{ active }">
             <b-button
               :label="operation"
@@ -18,91 +20,122 @@
             />
           </template>
 
-          <b-dropdown-item @click="changeOperation('add')" aria-role="listitem">Add</b-dropdown-item>
-          <b-dropdown-item  @click="changeOperation('sub')" aria-role="listitem">Subtract</b-dropdown-item>
-          <b-dropdown-item  @click="changeOperation('divide')" aria-role="listitem">Divide</b-dropdown-item>
-          <b-dropdown-item @click="changeOperation('mul')"  aria-role="listitem">Multiply</b-dropdown-item>
-
+          <b-dropdown-item @click="changeOperation('add')" aria-role="listitem"
+            >Add</b-dropdown-item
+          >
+          <b-dropdown-item @click="changeOperation('sub')" aria-role="listitem"
+            >Subtract</b-dropdown-item
+          >
+          <b-dropdown-item
+            @click="changeOperation('divide')"
+            aria-role="listitem"
+            >Divide</b-dropdown-item
+          >
+          <b-dropdown-item @click="changeOperation('mul')" aria-role="listitem"
+            >Multiply</b-dropdown-item
+          >
         </b-dropdown>
       </b-field>
 
-      <b-button v-if="buttonNoDekai"  @click="calculation()" type="is-primary">Calculate</b-button>
+      <b-button
+        v-if="buttonNoDekai && !buttonDisable"
+        @click="calculation()"
+        type="is-primary"
+        >Calculate</b-button
+      >
 
       <h1 v-if="result" class="title space">Result:{{ result }}</h1>
-
     </section>
   </div>
 </template>
 
 <script>
+import { Validator } from "simple-vue-validator";
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
-
+  validators: {
+    value1: function (value) {
+      let check = Validator.value(value).required()._messages;
+      if (check.length == 0) {
+        this.buttonDisable = false;
+      } else {
+        this.buttonDisable = true;
+      }
+      return Validator.value(value).required();
+    },
+    value2: function (value) {
+      let check = Validator.value(value).required()._messages;
+      if (check.length == 0) {
+        this.buttonDisable = false;
+      } else {
+        this.buttonDisable = true;
+      }
+      return Validator.value(value).required();
+    },
+  },
   data() {
     return {
       value1: null,
       value2: null,
       result: null,
-      operation: 'Select Operaion',
-      buttonNoDekai: true
+      operation: "Select Operaion",
+      buttonNoDekai: true,
+      buttonDisable: false,
     };
   },
- 
-  
-  beforeMount(){
-      if(this.operation === "Select Operaion"){
-        this.buttonNoDekai = false
-      }
-  },
-  watch:{
-    operation(newValue, oldValue){
-      console.log(oldValue)
-      if(newValue === "Select Operaion"){
-        this.buttonNoDekai = false
-      }else{
-        this.buttonNoDekai = true
 
-      }
+  beforeMount() {
+    if (this.operation === "Select Operaion") {
+      this.buttonNoDekai = false;
     }
-
+  },
+  watch: {
+    operation(newValue, oldValue) {
+      console.log(oldValue);
+      if (newValue === "Select Operaion") {
+        this.buttonNoDekai = false;
+      } else {
+        this.buttonNoDekai = true;
+      }
+    },
   },
 
   methods: {
     calculation() {
-      switch(this.operation){
-        case 'Addition':
-      this.result +=  parseInt(this.value1) + parseInt(this.value2);
-      break;
-      case'Substraction':
-      this.result = parseInt(this.value1) - parseInt(this.value2);
-      break;
-       case 'Division':
-      this.result = parseInt(this.value1) / parseInt(this.value2);
-      break;
-      case'Multiply':
-      this.result = parseInt(this.value1) * parseInt(this.value2);
-      break;
+      switch (this.operation) {
+        case "Addition":
+          this.result += parseInt(this.value1) + parseInt(this.value2);
+          break;
+        case "Substraction":
+          this.result = parseInt(this.value1) - parseInt(this.value2);
+          break;
+        case "Division":
+          this.result = parseInt(this.value1) / parseInt(this.value2);
+          break;
+        case "Multiply":
+          this.result = parseInt(this.value1) * parseInt(this.value2);
+          break;
       }
-
     },
-    changeOperation(message){
-      switch(message) {
-  case 'add':
-      this.operation = 'Addition'
-    break;
-  case 'sub':
-      this.operation = 'Substraction'
-    break;
-      case 'divide':
-      this.operation = 'Division'
-    break;
-    case 'mul':
-      this.operation = 'Multiply'
-    break;
-}    }
+    changeOperation(message) {
+      switch (message) {
+        case "add":
+          this.operation = "Addition";
+          break;
+        case "sub":
+          this.operation = "Substraction";
+          break;
+        case "divide":
+          this.operation = "Division";
+          break;
+        case "mul":
+          this.operation = "Multiply";
+          break;
+      }
+    },
   },
 };
 </script>
@@ -125,5 +158,10 @@ a {
 }
 .space {
   padding-top: 20px;
+}
+.message {
+  background-color: RED;
+  border-radius: 4px;
+  font-size: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import SimpleVueValidation from 'simple-vue-validator';
 
+Vue.use(SimpleVueValidation);
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
